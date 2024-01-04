6eb2c3725998f6c15a54b5c987549a1dfcc487aa
Abhi <devopsbyabhi@gmail.com>
2024-01-04 08:35:10 +0530

    modules

--- main.tf
+++ main.tf
@@ -8,3 +8,22 @@ resource "aws_instance" "from-terraform" {
     description = var.ec2-description
   }
 }
+
+
+# Module Usage
+# while using modules try to comment the above code and run terraform init again and 
+# use the modules
+
+module "ec2_instance" {
+  source = "terraform-aws-modules/ec2-instance/aws"
+
+  name = "single-instance"
+
+  instance_type          = "m5x.large"
+  vpc_security_group_ids = ["sg-12345678"]
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
