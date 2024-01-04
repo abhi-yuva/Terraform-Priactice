6097970755fce5cc749cf26a084b3a2fe8d62077
Abhi <devopsbyabhi@gmail.com>
2024-01-04 08:35:34 +0530

    modules

--- main.tf
+++ main.tf
@@ -1,4 +1,5 @@
 resource "aws_instance" "from-terraform" {
+  # count = 2
   ami                    = var.os-name
   instance_type          = var.instance_type
   vpc_security_group_ids = var.sg_ids
--- outputs.tf
+++ outputs.tf
@@ -1,24 +1,34 @@
 output "ami" {
     description = "AMI of EC2 Machine"
+    # if you are using count value use the below code
+    # value = aws_instance.from-terraform[count.index].ami
     value = aws_instance.from-terraform.ami 
 }
 
 output "instance_id" {
     description = "Instance ID of EC2 Machine"
+    # if you are using count value use the below code
+    # value = aws_instance.from-terraform[count.index].id
     value = aws_instance.from-terraform.id
 }
 
 output "public_ip" {
     description = "Public IP of EC2 Machine"
+    # if you are using count value use the below code
+    # value = aws_instance.from-terraform[count.index].public_ip
     value = aws_instance.from-terraform.public_ip
 }
 
 output "private_ip" {
     description = "Private IP of EC2 Machine"
+    # if you are using count value use the below code
+    # value = aws_instance.from-terraform[count.index].private_ip
     value = aws_instance.from-terraform.private_ip
 }
 
 output "Instacne_ARN" {
+    # if you are using count value use the below code
+    # value = aws_instance.from-terraform[count.index].arn
     value = aws_instance.from-terraform.arn
   
 }
