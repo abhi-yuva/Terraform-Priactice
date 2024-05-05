bad6e1660e98e95dab2ebc5b98ec679bb6bda46b
Abhi <devopsbyabhi@gmail.com>
2024-05-05 14:57:18 +0530

    outputs and depends on

--- main.tf
+++ main.tf
@@ -1,3 +1,20 @@
+resource "aws_vpc" "my_vpc" {
+  cidr_block = "10.0.0.0/24"
+  tags = {
+    Name = "my-vpc"
+  }
+  
+}
+
+resource "aws_security_group" "my-security-group" {
+  vpc_id = aws_vpc.my_vpc.id
+  tags = {
+    Name = "my-security-group"
+  }
+  
+  depends_on = [ aws_vpc.my_vpc ]
+}
+
 resource "aws_instance" "my-ec2" {
   ami           = var.image
   instance_type = var.type
@@ -6,4 +23,8 @@ resource "aws_instance" "my-ec2" {
   tags = {
     Name = "Through Terraform"
   }
+
+  depends_on = [ 
+    aws_security_group.my-security-group
+   ]
 }
