237137195ff62129f5a04118719d06a7012f9c76
Abhi <devopsbyabhi@gmail.com>
2024-05-06 07:07:12 +0530

    S3 Bucket Creation

--- main.tf
+++ main.tf
@@ -1,30 +1,38 @@
-resource "aws_vpc" "my_vpc" {
-  cidr_block = "10.0.0.0/24"
+resource "aws_s3_bucket" "my-bucket" {
+  bucket = "terraform-backend-bucket"
+
   tags = {
-    Name = "my-vpc"
+    Name        = "Bucket from Terraform"
   }
+}
+
+# resource "aws_vpc" "my_vpc" {
+#   cidr_block = "10.0.0.0/24"
+#   tags = {
+#     Name = "my-vpc"
+#   }
   
-}
+# }
 
-resource "aws_security_group" "my-security-group" {
-  vpc_id = aws_vpc.my_vpc.id
-  tags = {
-    Name = "my-security-group"
-  }
+# resource "aws_security_group" "my-security-group" {
+#   vpc_id = aws_vpc.my_vpc.id
+#   tags = {
+#     Name = "my-security-group"
+#   }
   
-  depends_on = [ aws_vpc.my_vpc ]
-}
+#   depends_on = [ aws_vpc.my_vpc ]
+# }
 
-resource "aws_instance" "my-ec2" {
-  ami           = var.image
-  instance_type = var.type
+# resource "aws_instance" "my-ec2" {
+#   ami           = var.image
+#   instance_type = var.type
 
 
-  tags = {
-    Name = "Through Terraform"
-  }
+#   tags = {
+#     Name = "Through Terraform"
+#   }
 
-  depends_on = [ 
-    aws_security_group.my-security-group
-   ]
-}
+#   depends_on = [ 
+#     aws_security_group.my-security-group
+#    ]
+# }
